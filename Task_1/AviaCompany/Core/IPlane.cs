646eb55ac5068633db176be24f3f6172a268a44a
SePiat <SePiat@list.ru>
2020-12-05 12:55:15 +0300

    CreateSolutionAviaCompanyEndGitIgnore

---
+++ Task_1/AviaCompany/Core/IPlane.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AviaCompany.Core
+{
+    public interface IPlane
+    {
+
+        public string ModelName { get; set; }
+        public string FlightNumber { get; set; }
+        public string Manufacturer { get; set; }
+        public string EngineType { get; set; }
+        public int Speed { get; set; }
+        public int MaxAltitude { get; set; }
+        public int FuelConsumption { get; set; }       
+        
+
+    }
+}
---
+++ Task_1/AviaCompany/Planes/Plane.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AviaCompany.Planes
+{
+    abstract class Plane
+    {
+
+       /* +abstract ModelName:string
++abstract FlightNumber
++abstract Manufacturer:string
++abstract EngineType:string
++abstract FlightRange:int
++abstract Speed:int
++abstract MaxAltitude:int
++abstract FuelConsumption:int*/
+
+    }
+}
